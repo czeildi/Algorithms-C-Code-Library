91611ebcadf781624ef7342394a33fc36c695b13
Czeller Ildikó <czeildi@users.noreply.github.com>
2015-03-14 00:34:15 +0100

    for loop simplified

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -33,25 +33,26 @@ class edge {
     return (w>e.w); //because priority_queue starts with highest element, but we prefer cheapest
   }
 };
+
 vector<vector<edge> > graph;
 int N;
+
 /*Dijkstra: single source shortest paths from vertex with id s
   in a graph with non-negative weights */
 void dijkstra(int s, vector<point> & vertices) {
-  vertices[s].d=0;
+  vertices[s].d = 0;
   priority_queue<point> q; //in increasing order of known distance from s
   q.push(vertices[s]);
   while(!q.empty()) {
-    point p=q.front();
-    q.pop();
+    point p = q.front(); q.pop();
+    
     if(!vertices[p.id].visited) {
-      vertices[p.id].visited=true;
-      for(unsigned int i=0; i<graph[p.id].size(); i++) {
-        edge e=graph[p.id][i];
-        if(!vertices[e.v].visited && vertices[e.v].d>e.w+vertices[p.id].d) {
-          vertices[e.v].d=e.w+vertices[p.id].d;
-          vertices[e.v].parent=p.id;
-          q.push(vertices[e.v]);
+      vertices[p.id].visited = true;
+      for(auto e = graph[p.id].begin(); e != graph[p.id].end(); e++) {
+        if(!vertices[e->v].visited && vertices[e->v].d > (e->w + vertices[p.id].d) ) {
+          vertices[e->v].d = e->w + vertices[p.id].d;
+          vertices[e->v].parent = p.id;
+          q.push(vertices[e->v]);
         }
       }
     }
